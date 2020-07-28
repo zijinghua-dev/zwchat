e51b2845445ba155d9fdf05ef36245d50f847ee7
冯亮 <feng.liang@evebit.cn>
2020-07-28 09:34:03 +0800

    修改配置文件

--- publishable/config/wechat.php
+++ publishable/config/wechat.php
@@ -3,9 +3,9 @@ return [
     //appid
     'apps' => [
         [
-            'app_id' => 'wxc6aecc4a0df02cda',
-            'redirect_uri' => 'http://local.d2p.shop:8000',
-            'app_secret' => '1c558b4423db984be0cc3847a86b64f1'
+            'app_id' => '',
+            'redirect_uri' => '',
+            'app_secret' => ''
         ]
     ],
 ];
